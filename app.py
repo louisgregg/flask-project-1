b6f96ea3f8766f72aa64e7629df80edf5f2a7f6e
Louis Gregg <fin@ocf.berkeley.edu>
2019-07-06 21:07:26 -0700

    cleaned up app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 #!/home/f/fi/fin/public_html/flasky/venv_3/bin/python3
+
 from flask import Flask, render_template,  send_from_directory, flash, redirect, url_for, session, logging, request
 from flask_project_1.make_tree import make_tree # For file trees in a tuple
 from flask_mysqldb import MySQL
@@ -8,35 +9,10 @@ import configparser
 import flask_project_1.pseudo_diceware as p_d
 from flask_project_1.make_glob_list import make_glob # for listing out files in a folder to pass to WT Forms. REQUIRES PYTHON3
 import re
+
 # Import variables from the config file.
 imported_config = configparser.ConfigParser()
 imported_config.read(("/home/f/fi/fin/public_html/flasky/flask_project_1/flaskapp-config-1.ini"))
-
-#import logging
-#logging.basicConfig(filename='error.log',level=logging.DEBUG)
-
-#logger = logging.getLogger('werkzeug')
-#handler = logging.FileHandler('access.log')
-#logger.addHandler(handler)
-
-# Also add the handler to Flask's logger for cases
-#  where Werkzeug isn't used as the underlying WSGI server.
-#app.logger.addHandler(handler)
-
-# from flask import Flask, render_template,  send_from_directory, flash, redirect, url_for, session, logging, request
-# from make_tree import make_tree # For file trees in a tuple
-# from flask_mysqldb import MySQL
-# from wtforms import Form, StringField, SelectField, IntegerField, TextAreaField, PasswordField, validators
-# from passlib.hash import sha256_crypt
-# import configparser
-# import pseudo_diceware
-# from make_glob_list import make_glob # for listing out files in a folder to pass to WT Forms. REQUIRES PYTHON3
-# import re
-# # Import variables from the config file.
-# imported_config = configparser.ConfigParser()
-# imported_config.read(("flaskapp-config-1.ini"))
-
-
 
 app =Flask(__name__)
 
@@ -58,11 +34,6 @@ app.secret_key=imported_config['DEFAULT']['session_secretkey']
 def index():
     return render_template('home.html')
 
-## Don't currently need an about page.
-# @app.route('/about')
-# def about():
-#     return render_template('about.html')
-
 @app.route('/music')
 def music():
     # Route for the music page
@@ -72,97 +43,6 @@ def music():
 @app.route("/media/<string:path>")
 def media_send(path):
     return send_from_directory("media", path)
-
-# class RegisterForm(Form):
-# 	name = StringField('Name', [validators.Length(min=1,max=50)])
-# 	email = StringField('Email', [validators.DataRequired(), validators.Length(min=6, max=50), validators.Email(message='Please enter a valid email.')])
-# 	username= StringField('Username', [validators.DataRequired(), validators.Length(min=4,max=25)])
-# 	password = PasswordField('Password', [validators.DataRequired(), validators.EqualTo('confirm', message='Passwords do not match.')])
-# 	confirm = PasswordField('Comfirm Password',[validators.DataRequired()])
-#
-# @app.route('/register', methods=['GET', 'POST'] )
-# def register():
-# 	form = RegisterForm(request.form)
-# 	if request.method == 'POST' and form.validate():
-# 		# Do stuff if post request is made
-# 		name=form.name.data
-# 		email=form.email.data
-# 		username=form.username.data
-# 		"""
-# 		THe password variable being filled below is in fact the concatination of a salt generated once and the hash of this salt with the password supplied by the user. This hash string is pushed to the MySQL DB.
-# 		I changed this to sha256_crypt.hash from sha256_crypt.encrypt as it was deprecated.
-# 		"""
-# 		password=sha256_crypt.hash(str(form.password.data))
-#
-# 		# Create cursor
-# 		cur = mysql.connection.cursor()
-#
-# 		# Execute query
-# 		cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
-#
-# 		# Commit to DB
-# 		mysql.connection.commit()
-#
-# 		# Close connection
-# 		cur.close()
-#
-# 		flash('You are now registered and can log in', 'success')
-#
-# 		return redirect(url_for('login'))
-# 	return render_template('register.html', form=form)
-#
-# # User login
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     if request.method == 'POST':
-#         # Get Form Fields
-#         username = request.form['username']
-#         password_candidate = request.form['password']
-#
-#         #create cursor
-#         cur = mysql.connection.cursor()
-#
-#         # Get user by Username
-#         result = cur.execute("SELECT * FROM users WHERE username = %s", [username])
-#
-#         if result > 0:
-#             #Get stored hash
-#             data = cur.fetchone()
-#             password = data['password']
-#
-#             #compare passwords
-#             if sha256_crypt.verify(password_candidate, password):
-#                 #if  correct password
-#                 session['logged_in'] = True
-#                 session['username'] = username
-#
-#                 flash('You are now logged in', 'success')
-#                 return redirect(url_for('dashboard'))
-#
-#             else :
-#                 #if password incorrect
-#                 app.logger.info('Valid username. Invalid Password.')
-#                 error = 'Invalid Login'
-#                 return render_template('login.html', error=error)
-#
-#         else : #username not found
-#             app.logger.info('No User')
-#             error = 'Username not found'
-#             return render_template('login.html', error=error)
-#         cur.close()
-#     return render_template('login.html')
-#
-# # Logout
-# @app.route('/logout')
-# def logout():
-#     session.clear()
-#     flash('You are now logged out', 'success')
-#     return redirect(url_for('login'))
-#
-# # Dashboard
-# @app.route('/dashboard')
-# def dashboard():
-#     return render_template('dashboard.html')
 
 # Produce a dict of previously generated passwords to display in each web page
 def retrieve_generated_passwords(n_passwords):
@@ -212,18 +92,14 @@ def diceware():
         # Create cursor
         cur = mysql.connection.cursor()
         # Execute query
-        #cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
         cur.execute("INSERT INTO generated_passwords(ip_address, browser_info, generated_password, book) VALUES(%s,%s,%s,%s)",(client_ip, user_agent, diceware_passphrase, book_used))
         # Commit to DB
         mysql.connection.commit()
         # Close connection
         cur.close()
 
-    ####
     #### Retrieve previously generated passwrods to tabulate on the page.
-    ####
     dict_of_passphrase_tuples = retrieve_generated_passwords(100)
-    #app.logger.info(dict_of_passphrase_tuples)
     return render_template('diceware.html', form=form, dict_of_tuples=dict_of_passphrase_tuples)
 
 @app.route('/nlp')
@@ -240,16 +116,6 @@ def sentiment_images_send(path):
 def photos_send(path):
     return send_from_directory("photos", path)    
 
-
-@app.route('/password_list')
-def password_list():
-    ####
-    #### Retrieve previously generated passwrods to tabulate on the page.
-    ####
-    dict_of_passphrase_tuples = retrieve_generated_passwords(100)
-    app.logger.info(dict_of_passphrase_tuples)
-    return render_template('password_list.html', dict_of_tuples=dict_of_passphrase_tuples)
-
 @app.route('/contact')
 def contact():
     return render_template('contact.html') 
@@ -261,7 +127,4 @@ def key_send(path):
 # Initialization at runtime from command line
 if __name__ == '__main__':
     app.secret_key=imported_config['DEFAULT']['session_secretkey']
-    #import logging
-    #logging.basicConfig(filename='error.log',level=logging.DEBUG)
-    
     app.run()
